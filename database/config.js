c5998b0dfa24454f459e902e86744354419971d8
Martin <martinquesquenpa3@gmail.com>
2021-12-06 14:59:18 -0500

    cambiando

--- database/config.js
+++ database/config.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 //const urlFinal="mongodb://localhost:27017/apiAngeles";
-// const urlFinal="mongodb+srv://root:root@cluster0.gobb5.mongodb.net/apiBilleteraMorris?retryWrites=true&w=majority";
-const urlFinal="mongodb+srv://mquesquenpa3:Martin123456@cluster0.jw2qw.mongodb.net/Peluqueria?retryWrites=true&w=majority";
+const urlFinal="https://aqui va la url";
 
 const dbConnection = async() =>{
     try {
